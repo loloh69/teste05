6c38e5204ebc7e6eafcbffdeb2cd03744763149a
loloh69 <lohany69ofc@gmail.com>
2025-11-11 17:58:54 -0400

    Add files via upload

---
+++ src/animar.js
@@ -0,0 +1,25 @@
+function criarPinto() {
+  const container = document.querySelector('#chuva');
+  const pinto = document.createElement('img');
+  pinto.src = 'icon.png';
+  pinto.classList.add('pintos');
+
+
+  // Posição horizontal aleatória
+  pinto.style.left = Math.random() * window.innerWidth + 'px';
+
+
+  // Duração aleatória (opcional)
+  const duracao = 2 + Math.random() * 3;
+  pinto.style.animationDuration = duracao + 's';
+
+  container.appendChild(pinto);
+
+  // Remover após a animação
+  setTimeout(() => {
+    container.removeChild(pinto);
+  }, duracao * 1000);
+}
+
+// Criar corações continuamente
+setInterval(criarPinto, 500);
